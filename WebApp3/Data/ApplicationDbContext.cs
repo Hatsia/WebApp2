6801869f99bb1d32644bb1e9b196eabdda159c41
Ivan Serdyukov <ivan.serdyukov270@gmail.com>
2022-06-08 14:20:26 +0300

    Добавьте файлы проекта.

---
+++ WebApp3/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApp3.Models;
+
+namespace WebApp3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebApp3.Models.Product> Product { get; set; }
+    }
+}
