05cd9dc35e2477a5d3f42fe15ee62545f3c15712
Ivan Serdyukov <ivan.serdyukov270@gmail.com>
2022-06-08 14:20:26 +0300

    Add Product category, simple Search by name of product

---
+++ WebApp3/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp3.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Product()
+        {
+
+        }
+    }
+}
